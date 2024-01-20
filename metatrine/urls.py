1e653758c14b8dc93dfbdae47aa33489f08613e4
Matin <matin@Matins-MacBook-Air.local>
2024-01-21 02:46:11 +0300

    initial commit

--- metatrine/urls.py
+++ metatrine/urls.py
@@ -18,12 +18,10 @@ urlpatterns = [
 
 if settings.DEBUG:
     from django.conf.urls.static import static
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-    # Serve static and media files from development server
-    urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
+    
 urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
@@ -34,3 +32,9 @@ urlpatterns = urlpatterns + [
     #    path("pages/", include(wagtail_urls)),
 ]
 
+# These pieces of code has been deleted because of AWS EC2
+    # from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    # Serve static and media files from development server
+    # urlpatterns += staticfiles_urlpatterns()
+    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

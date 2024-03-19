5dc74af243dbc6d7b8e5fdd06e3bac8b60d5073d
Matin <matin@Matins-MacBook-Air.local>
2024-03-20 01:45:08 +0300

    initial commit

--- blog/admin.py
+++ blog/admin.py
@@ -1,17 +1,2 @@
 from django.contrib import admin
-<<<<<<< HEAD
-<<<<<<< HEAD
-# Register your models here.
-=======
-# Register your models here.
-=======
-# Register your models here.
->>>>>>> origin/master
 
-
-
-
-<<<<<<< HEAD
->>>>>>> 9a20156 (initial commit)
-=======
->>>>>>> origin/master
